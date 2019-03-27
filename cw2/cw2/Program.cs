8564593914f3bbcb3efc1bc7c6756eb8bc4f4dfa
wojprz <wojprz@st.amu.edu.pl>
2019-03-27 15:18:44 +0100

    update zadan

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -1,12 +1,174 @@
-﻿using System;
+﻿//Wojciech Przybyła
+//Cw 2
+//C#
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace cw2
 {
     class Program
     {
+        static void Start()
+        {
+            Console.SetWindowSize(Math.Min(110, Console.LargestWindowWidth), Math.Min(35, Console.LargestWindowHeight));
+        }
+        static double[] GetVector(double value)
+        {
+            double[] vector = new double[26];
+            for (int i=0; i<26; i++)
+            {
+                vector[i] = value;
+            }
+            return vector;
+        }
+        static double sygNaucz(double t)
+        {
+            if (1 + ((t - 1) % 5) <= 3)
+                return 1.0;
+            else if (1 + ((t - 1) % 5) > 3)
+                return 0.0;
+            else
+                return 2.0;
+        }
+        static string viewDouble(double value)
+        {
+
+            CultureInfo USFormat = new CultureInfo("en-US");
+            return value.ToString("0.00", USFormat);
+        }
+        static List<double[]> wejscie()
+        {
+            double[] u1 = new double[26];
+            u1 = GetVector(0.0);
+            u1[6] = 1.0;
+            u1[7] = 1.0;
+            u1[12] = 1.0;
+            u1[17] = 1.0;
+            u1[23] = 1.0;
+            u1[25] = 1.0;
+
+            double[] u2 = new double[26];
+            u2 = GetVector(0.0);
+            u2[2] = 1.0;
+            u2[3] = 1.0;
+            u2[8] = 1.0;
+            u2[13] = 1.0;
+            u2[25] = 1.0;
+
+            double[] u3 = new double[26];
+            u3 = GetVector(0.0);
+            u3[5] = 1.0;
+            u3[6] = 1.0;
+            u3[11] = 1.0;
+            u3[16] = 1.0;
+            u3[22] = 1.0;
+            u3[25] = 1.0;
+
+            double[] u4 = new double[26];
+            u4 = GetVector(0.0);
+            u4[6] = 1.0;
+            u4[7] = 1.0;
+            u4[8] = 1.0;
+            u4[11] = 1.0;
+            u4[13] = 1.0;
+            u4[16] = 1.0;
+            u4[17] = 1.0;
+            u4[18] = 1.0;
+            u4[25] = 1.0;
+
+            double[] u5 = new double[26];
+            u5 = GetVector(0.0);
+            u5[10] = 1.0;
+            u5[11] = 1.0;
+            u5[12] = 1.0;
+            u5[15] = 1.0;
+            u5[17] = 1.0;
+            u5[20] = 1.0;
+            u5[21] = 1.0;
+            u5[22] = 1.0;
+            u5[25] = 1.0;
+
+            List<double[]> u = new List<double[]>() { u1, u2, u3, u4, u5 };
+            return u;
+        }
+        static double iloSkal(double[] w, double[] u)
+        {
+            double y = 0.0;
+            for(int i=0; i<26; i++)
+            {
+                y += w[i] * u[i];
+            }
+            if (y >= 0)
+                return 1.0;
+            else
+                return 0.0;
+        }
+        static void algorytm(double c)
+        {
+            int licznik = 0;
+            int t = 1;
+            double z;
+            double y;
+
+            double[] w = new double[26];
+            w = GetVector(1.0);
+
+            List<double[]> u = new List<double[]>();
+            u = wejscie();
+
+            while(licznik != 5)
+            {
+                z = sygNaucz(t);
+                y = iloSkal(w, u[(t - 1) % 5]);
+
+                for(int i=0; i<26; i++)
+                {
+                    w[i] = w[i] + c * (z - y) * u[(t - 1) % 5][i];
+                }
+
+                t += 1;
+
+                if (z == y)
+                    licznik++;
+                else
+                    licznik = 0;
+            }
+
+            Console.WriteLine("c= " + c + "," + "t= " + t);
+            Console.WriteLine("******************************************************");
+
+            int id = 0;
+            for(int r = 1; r<6; r++)
+            {
+                for(int co = 1; co<6; co++)
+                {
+                    Console.Write(viewDouble(w[id]) + "\t");
+                    id++;
+                }
+                if(r == 5)
+                    Console.WriteLine(viewDouble(w[25]));
+
+                Console.WriteLine();
+
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Start();
+            algorytm(0.01);
+            Console.WriteLine();
+
+            algorytm(0.1);
+            Console.WriteLine();
+
+            algorytm(1.0);
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadKey();
+         
         }
     }
 }
---
+++ cw3/cw3/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace cw3
+{
+    class Program
+    {
+        static void Start()
+        {
+            Console.SetWindowSize(Math.Min(110, Console.LargestWindowWidth), Math.Min(35, Console.LargestWindowHeight));
+        }
+        static double[] GetVector(double value)
+        {
+            double[] vector = new double[25];
+            for (int i = 0; i < 25; i++)
+            {
+                vector[i] = value;
+            }
+            return vector;
+        }
+        static double[] getZ0 (string type)
+        {
+            double[] z0 = new double[25];
+            z0 = GetVector(-1.0);
+
+            if (type == "normalny")
+            {
+                z0[6] = 1.0;
+                z0[7] = 1.0;
+                z0[8] = 1.0;
+                z0[11] = 1.0;
+                z0[13] = 1.0;
+                z0[16] = 1.0;
+                z0[17] = 1.0;
+                z0[18] = 1.0;
+            }
+            else if(type == "zaburzony")
+            {
+                z0[1] = 1.0;
+                z0[2] = 1.0;
+                z0[3] = 1.0;
+                z0[6] = 1.0;
+                z0[8] = 1.0;
+                z0[11] = 1.0;
+                z0[13] = 1.0;
+                z0[16] = 1.0;
+                z0[17] = 1.0;
+                z0[18] = 1.0;
+            }
+            return z0;
+        }
+        static double[] getZ1 (string type)
+        {
+            double[] z1 = new double[25];
+            z1 = GetVector(-1.0);
+
+            if (type == "normalny")
+            {
+                z1[6] = 1.0;
+                z1[7] = 1.0;
+                z1[12] = 1.0;
+                z1[17] = 1.0;
+            }
+            else if(type == "zaburzony")
+            {
+                z1[2] = 1.0;
+                z1[7] = 1.0;
+                z1[12] = 1.0;
+                z1[17] = 1.0;
+                z1[22] = 1.0;
+            }
+            return z1;
+        }
+        static double[][] matrixW(double[] z0, double[] z1)
+        {
+            double[][] W = new double[25][];
+            for (int i = 0; i < 25; i++)
+            {
+                W[i] = new double[25];
+            }
+
+            for(int i = 0; i < 25; i++)
+            {
+                for (int j = 0; j < 25; j++)
+                {
+                    W[i][j] = 1.0 / 25.0 * (z0[i] * z0[j] + z1[i] * z1[j]);
+                }
+            }
+            return W;
+        }
+        static double[] SGN (double[] x)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                if (x[i] >= 0) x[i] = 1.0;
+                else if (x[i] < 0) x[i] = -1.0;
+            }
+            return x;
+        }
+        static double[] f (double[][] w, double[] u)
+        {
+            double[] y = new double[25];
+            double wartoscY = 0.0;
+
+            for(int i = 0; i < 25; i++)
+            {
+                for (int j = 0; j < 25; j++)
+                {
+                    wartoscY += w[i][j] * u[j];
+                }
+                y[i] = wartoscY;
+                wartoscY = 0.0;
+            }
+            y = SGN(y);
+            return y;
+        }
+        static void wyswietlanie(double[] z)
+        {
+            for(int i = 0; i < 25; i++)
+            {
+                if (z[i] == -1.0) Console.Write(" [ ] ");
+                else Console.Write(" [*] ");
+
+                if (i % 5 == 4) Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Start();
+            double[] z0 = getZ0("normalny");
+            double[] z0pri = getZ0("zaburzony");
+
+            double[] z1 = getZ1("normalny");
+            double[] z1pri = getZ1("zaburzony");
+
+            double[][] W = matrixW(z0, z1);
+            double[] y;
+
+            //z0
+            Console.WriteLine("z0\tF(z0)");
+            Console.WriteLine("***********************************************************");
+            y = f(W, z0);
+            wyswietlanie(y);
+            Console.WriteLine();
+            //z1
+            Console.WriteLine("z1\tF(z1)");
+            Console.WriteLine("***********************************************************");
+            y = f(W, z1);
+            wyswietlanie(y);
+            Console.WriteLine();
+            //z0 zaburzone
+            Console.WriteLine("z0 zaburzone");
+            Console.WriteLine("***********************************************************");
+            wyswietlanie(z0pri);
+            Console.WriteLine();
+            //z0 zaburzone wynik
+            Console.WriteLine("z0 (wynik zaburzonego)\tF(z0)");
+            Console.WriteLine("***********************************************************");
+            y = f(W, z0pri);
+            wyswietlanie(y);
+            Console.WriteLine();
+           
+            //z1 zaburzone
+            Console.WriteLine("z1 zaburzone");
+            Console.WriteLine("***********************************************************");
+            wyswietlanie(z1pri);
+            Console.WriteLine();
+            //z1 zaburzone wynik
+            Console.WriteLine("z1 (wynik zaburzonego)\tF(z1)");
+            Console.WriteLine("***********************************************************");
+            y = f(W, z1pri);
+            wyswietlanie(y);
+            Console.WriteLine();
+            
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
