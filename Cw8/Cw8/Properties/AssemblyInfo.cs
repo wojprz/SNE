2ebbc7333df2b243caa37296f744a518cfbfbed1
wojprz <wojprz@st.amu.edu.pl>
2019-05-15 16:30:41 +0200

    fix name

--- Cw8/Cw8/Properties/AssemblyInfo.cs
+++ Cw8/Cw8/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Ogólne informacje o zestawie są kontrolowane poprzez następujący 
 // zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
 // powiązane z zestawem.
-[assembly: AssemblyTitle("Cw8")]
+[assembly: AssemblyTitle("cw8")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cw8")]
+[assembly: AssemblyProduct("cw8")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
